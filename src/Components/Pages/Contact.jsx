4debe11c1747487317dcf6e12ca8d33260bd6405
byiringiroaimefils <aimefils173@example.com>
2024-12-17 14:07:49 +0200

    Fix:Fixing some error in order to handle some issues

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -15,8 +15,8 @@ export default function Contact() {
                     <div className='flex flex-col'>
                         <h3 className='text-white font-bold text-2xl'>Get in touch!</h3>
                         <p className='text-gray-200'>Contact us for any inquiries you might have</p> <br />
-                        <input type="text" placeholder='Email Address' className='bg-gray-200 outline-none p-2' /> <br />
-                        <textarea name="messae" placeholder='Type your Message' className=' bg-gray-200 p-2 outline-none '></textarea> <br />
+                        <input type="text" placeholder='Email Address' className='bg-white outline-none p-2' /> <br />
+                        <textarea name="messae" placeholder='Type your Message' className=' bg-white p-2 outline-none '></textarea> <br />
                         <button
                             className="w-full md:w-auto px-8 py-3 bg-white  font-semibold rounded-lg  transition-colors duration-200 text-center">
                             Submit your Meassage
